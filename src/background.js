a0a81a0a852804ad85308733e08d04be1613e222
kay-adamof <57440326+kay-adamof@users.noreply.github.com>
2023-02-18 08:33:23 +0900

    🌱

--- src/background.js
+++ src/background.js
@@ -1,8 +1,4 @@
 importScripts('./third-party/clipboard.min.js');
 
-var clipboard = new ClipboardJS(button, {
-    text: function() {
-        return chats[0].textContent;
-    }
-});
+var clipboard = new ClipboardJS(button);
 
